aa21d29602351e75202e2c7483c9cd69ae8b0b2e
Andrew Polkinghorn <apolkinghorn@scottlogic.co.uk>
2017-04-14 15:27:11 +0100

    Changes the directory we copy the framework structure from.

--- post-script.sh
+++ post-script.sh
@@ -12,7 +12,7 @@ mkdir -p "$OUTPUTFOLDER"
 rm -rf "$OUTPUTFOLDER"/*
 
 # Step 1. Copy the framework structure (from iphoneos build) to the universal folder
-cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework" "$OUTPUTFOLDER"
+cp -R "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework" "$OUTPUTFOLDER"
 
 # Step 2. Create universal binary file using lipo and place the combined executable in the copied framework directory
 lipo -create -output "$OUTPUTFOLDER/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework/${PROJECT_NAME}"
