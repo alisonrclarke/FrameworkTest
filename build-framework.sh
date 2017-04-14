ac1559c9859afc5897831020b70763775acc7ab6
Andrew Polkinghorn <apolkinghorn@scottlogic.co.uk>
2017-04-14 11:43:49 +0100

    Fix build framework script refencing old variables.

--- build-framework.sh
+++ build-framework.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # Step 1. Build Device and Simulator versions on iOS
-xcodebuild -project "${PROJECT_NAME}.xcodeproj" -scheme "${PROJECT_NAME}"  -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6' clean build
-#xcodebuild -scheme "${PROJECT_NAME}" -sdk iphoneos clean build
+xcodebuild -project "Test.xcodeproj" -scheme "Test"  -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6' clean build
 
