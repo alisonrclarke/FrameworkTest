99712c1ab077801f02bf1cebc4d9baec5f7107ac
Andrew Polkinghorn <apolkinghorn@scottlogic.co.uk>
2017-04-19 10:20:04 +0100

    Fixes version number in xcode proj & build script.

--- build-framework.sh
+++ build-framework.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Step 1. Build Device and Simulator versions on iOS
-xcodebuild -project "Test.xcodeproj" -scheme "Test"  -sdk iphonesimulator clean build
-xcodebuild -project "Test.xcodeproj" -scheme "Test"  -sdk iphoneos clean build
+xcodebuild -project "Test.xcodeproj" -scheme "Test"  -sdk iphonesimulator TEST_FRAMEWORK_VERSION="$TEST_FRAMEWORK_VERSION" clean build
+xcodebuild -project "Test.xcodeproj" -scheme "Test"  -sdk iphoneos TEST_FRAMEWORK_VERSION="$TEST_FRAMEWORK_VERSION" clean build
 
